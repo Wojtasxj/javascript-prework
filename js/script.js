a4579a1b859a8c565ff6b9aa229b782c1b7583c6
Wojciech Klause <wojtasxj@gmail.com>
2024-03-29 09:10:17 +0100

    changed if to switch

--- js/script.js
+++ js/script.js
@@ -10,39 +10,26 @@ document.getElementById('play-rock').addEventListener('click', function(){
     playGame(3);
   });
 
-/*dodane do implementacji guzikow*/
 function playGame(playerInput){
-    /* funkcja argMoveId -> getMoveName */
     clearMessages();
     function getMoveName(argMoveId){
-        if(argMoveId == 1){
-        return 'kamień';
-        } else if(argMoveId == 2){
-            return 'papier';
-        } else if(argMoveId == 3){
+        switch(argMoveId){
+            case 1:
+                return 'kamień';
+            case 2:
+                return 'papier';
+            case 3:
                 return 'nożyce';
-            } else { 
-        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-        return 'nieznany ruch';
+            default:
+                printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+                return 'nieznany ruch';
         }
     }
-
-    /* losowanie ruchu komputera */
-
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('Wylosowana liczba to: ' + randomNumber);
-    let argComputerMove = randomNumber;
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const argComputerMove = randomNumber;
     printMessage('Mój ruch to: ' + getMoveName(argComputerMove));
-
-    /* pobranie ruchu gracza */
-
-    /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    console.log('Gracz wpisał: ' + playerInput);*/
-    let  argPlayerMove = playerInput
+    const argPlayerMove = playerInput
     printMessage('Twój ruch bratku to: ' + getMoveName(argPlayerMove));
-
-    /* sprawdzenie w konsoli */
-    console.log('moves:', argComputerMove, argPlayerMove);
 
     function displayResult(computerMove, playerMove) {
         printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
