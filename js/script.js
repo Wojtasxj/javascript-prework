4db4f02a27a7cd41fc49524204fd00cf21403283
Wojciech Klause <wojtasxj@gmail.com>
2024-03-29 14:11:53 +0100

    functions rewritten

--- js/script.js
+++ js/script.js
@@ -1,49 +1,49 @@
-document.getElementById('play-rock').addEventListener('click', function(){
-    playGame(1);
-  });
-
-  document.getElementById('play-paper').addEventListener('click', function(){
-    playGame(2);
-  });
-  
-  document.getElementById('play-scissors').addEventListener('click', function(){
-    playGame(3);
-  });
-
-function playGame(playerInput){
-    clearMessages();
-    function getMoveName(argMoveId){
-        switch(argMoveId){
-            case 1:
-                return 'kamień';
-            case 2:
-                return 'papier';
-            case 3:
-                return 'nożyce';
-            default:
-                printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-                return 'nieznany ruch';
-        }
-    }
-    const randomNumber = Math.floor(Math.random() * 3 + 1);
-    const argComputerMove = randomNumber;
-    printMessage('Mój ruch to: ' + getMoveName(argComputerMove));
-    const argPlayerMove = playerInput
-    printMessage('Twój ruch bratku to: ' + getMoveName(argPlayerMove));
-
-    function displayResult(computerMove, playerMove) {
-        printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
-
-        if (playerMove === computerMove) {
-            printMessage('Remis');
-        } else if ((playerMove === 1 && computerMove === 3) ||
-                   (playerMove === 2 && computerMove === 1) ||
-                   (playerMove === 3 && computerMove === 2)) {
-            printMessage('Ty wygrywasz!');
-        } else {
-            printMessage('Komputer wygrywa');
-        }
-    }
+{
+    document.getElementById('play-rock').addEventListener('click', function(){
+        playGame(1);
+    });
     
-    displayResult(getMoveName(argComputerMove), getMoveName(argPlayerMove));
+    document.getElementById('play-paper').addEventListener('click', function(){
+        playGame(2);
+    });
+    
+    document.getElementById('play-scissors').addEventListener('click', function(){
+        playGame(3);
+    });
+    
+    function playGame(playerMove){
+        clearMessages();
+    
+        function getMoveName(argMoveId){
+            switch(argMoveId){
+                case 1:
+                    return 'kamień';
+                case 2:
+                    return 'papier';
+                case 3:
+                    return 'nożyce';
+                default:
+                    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+                    return 'nieznany ruch';
+            }
+        }
+    
+        const randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+        function displayResult(randomNumber, playerMove) {
+            printMessage('Zagrałem ' + getMoveName(randomNumber) + ', a Ty ' + getMoveName(playerMove));
+    
+            if (playerMove === randomNumber) {
+                printMessage('Remis');
+            } else if ((playerMove === 1 && randomNumber === 3) ||
+                    (playerMove === 2 && randomNumber === 1) ||
+                    (playerMove === 3 && randomNumber === 2)) {
+                printMessage('Ty wygrywasz!');
+            } else {
+                printMessage('Komputer wygrywa');
+            }
+        }
+        
+        displayResult(randomNumber, playerMove);
+    }
 }
